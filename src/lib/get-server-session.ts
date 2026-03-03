03d87c5f9e6a3367735ee2d226ae7c3564dddb63
BILLYCHARMANT <billycharmant541@gmail.com>
2026-03-03 20:17:49 +0200

    error correction

--- src/lib/get-server-session.ts
+++ src/lib/get-server-session.ts
@@ -7,9 +7,3 @@ import type { NextAuthOptions } from "next-auth";
 export async function getServerSession(options: NextAuthOptions) {
   return nextAuthGetServerSession(options);
 }
-
-
-Role	Username	Email	Password
-SUPERADMIN	superadmin	superadmin@difarm.local	SuperAdmin1
-ADMIN	admin	admin@difarm.local	AdminPass1
-MANAGER	manager	manager@difarm.local	ManagerPass1
